d87ceff32048473fc46a1894afaa71f2b75fb0fe
jeffsanchez <sanchez.christopherjeff@gmail.com>
2022-03-22 20:56:47 +0800

    feat: Initial realtime sta-lta on SL connx

---
+++ STALTA-trigger.py
@@ -0,0 +1,50 @@
+from obspy.clients.seedlink.easyseedlink import create_client
+from obspy.realtime import RtTrace
+from collections import deque
+import numpy as np
+import matplotlib.pyplot as plt
+
+plt.axis([0, 10, 0, 1])
+
+# derived from SC3's scautopick default config
+sta_window = 2 # seconds
+lta_window = 10 #80
+ratio_window = 120
+thres_on = 3
+thres_off = 1.5
+
+rt_trace = RtTrace(max_length=ratio_window)
+
+#ratio = deque([0]*(ratio_window*100)) # assume 100Hz
+ratio = np.zeros(ratio_window*100) # assume 100Hz
+
+def append_trace_to_realtime(tr):
+    global ratio
+
+    print("Appending the following trace:")
+    print(tr)
+    rt_trace.append(tr)
+
+
+    print("RtTrace:")
+    print(rt_trace)
+
+    print("Calculating STA/LTA")
+    sta_lta = rt_trace.copy().trigger("recstalta", sta=sta_window, lta=lta_window)
+    for sample in sta_lta.data:
+        ratio = np.roll(ratio,-1)        #ratio.popleft()
+        ratio[-1] = sample        #ratio.append(sample)
+    print()
+
+    plt.plot(ratio)
+    plt.draw()
+    plt.pause(0.0001)
+    plt.clf()
+
+
+
+
+
+client = create_client("10.196.16.147", on_data=append_trace_to_realtime)
+client.select_stream("AM", "RE722", "EHZ")
+client.run()
