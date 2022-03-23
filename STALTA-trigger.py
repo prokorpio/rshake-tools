7b10aed9a6a7246f968ac04f652ce495599cd6ce
jeffsanchez <sanchez.christopherjeff@gmail.com>
2022-03-23 14:16:28 +0800

    feat: Add live plot for mseed trace

--- STALTA-trigger.py
+++ STALTA-trigger.py
@@ -4,7 +4,7 @@ from collections import deque
 import numpy as np
 import matplotlib.pyplot as plt
 
-plt.axis([0, 10, 0, 1])
+fig, axs = plt.subplots(2)
 
 # derived from SC3's scautopick default config
 sta_window = 2 # seconds
@@ -15,7 +15,6 @@ thres_off = 1.5
 
 rt_trace = RtTrace(max_length=ratio_window)
 
-#ratio = deque([0]*(ratio_window*100)) # assume 100Hz
 ratio = np.zeros(ratio_window*100) # assume 100Hz
 
 def append_trace_to_realtime(tr):
@@ -36,10 +35,12 @@ def append_trace_to_realtime(tr):
 
     print()
 
-    plt.plot(ratio)
-    plt.draw()
+    axs[0].plot(rt_trace.data)
+    axs[1].plot(ratio)
+    fig.canvas.draw_idle()
     plt.pause(0.0001)
-    plt.clf()
+    axs[0].cla()
+    axs[1].cla()
 
 client = create_client("10.196.16.147", on_data=append_trace_to_realtime)
 client.select_stream("AM", "RE722", "EHZ")
