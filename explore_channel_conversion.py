5431c505c4738979b87ad87d75c9235551fb89cb
jeffsanchez <sanchez.christopherjeff@gmail.com>
2022-04-27 16:56:36 +0800

    feat: Add function for determining low corner freq

--- explore_channel_conversion.py
+++ explore_channel_conversion.py
@@ -2,7 +2,10 @@ from obspy import read_inventory
 from obspy.core import read
 from obspy.clients.fdsn import Client as RS_Client
 from obspy.core.inventory import Inventory, Network
-from obspy.signal.invsim import estimate_magnitude
+#from obspy.signal.invsim import estimate_magnitude
+from obspy.signal.trigger import recursive_sta_lta, trigger_onset, plot_trigger
+from obspy.signal.util import _npts2nfft
+from scipy import integrate
 from pathlib import Path
 import matplotlib.pyplot as plt
 import os
@@ -11,20 +14,33 @@ import warnings
 
 INV_DIR = "inventories"
 event_type = 1
-mseed_file     = "/home/jeffsanchez/Downloads/cosmos_downloads/Event_1/evid_15200401_13519/SB_WLA_HNZ_00_15200401.msd"
-prism_v1_file  = "/home/jeffsanchez/Downloads/cosmos_downloads/Event_1/prismPROCESSED/CI.15200401/SB.WLA/V1/SB_WLA_HNZ_00_15200401.V1c"
-prism_acc_file = "/home/jeffsanchez/Downloads/cosmos_downloads/Event_1/prismPROCESSED/CI.15200401/SB.WLA/V2/SB_WLA_HNZ_00_15200401.acc.V2c"
-prism_vel_file = "/home/jeffsanchez/Downloads/cosmos_downloads/Event_1/prismPROCESSED/CI.15200401/SB.WLA/V2/SB_WLA_HNZ_00_15200401.vel.V2c"
-prism_dis_file = "/home/jeffsanchez/Downloads/cosmos_downloads/Event_1/prismPROCESSED/CI.15200401/SB.WLA/V2/SB_WLA_HNZ_00_15200401.dis.V2c"
-mseed_file     = "/home/jeffsanchez/Downloads/cosmos_downloads/Event_3/evid_10665149_4386/SB_WLA_HNZ_00_10665149.msd"
-prism_v1_file  = "/home/jeffsanchez/Downloads/cosmos_downloads/Event_3/prismPROCESSED/CI.10665149/SB.WLA/V1/SB_WLA_HNZ_00_10665149.V1c"
-prism_acc_file = "/home/jeffsanchez/Downloads/cosmos_downloads/Event_3/prismPROCESSED/CI.10665149/SB.WLA/V2/SB_WLA_HNZ_00_10665149.acc.V2c"
-prism_vel_file = "/home/jeffsanchez/Downloads/cosmos_downloads/Event_3/prismPROCESSED/CI.10665149/SB.WLA/V2/SB_WLA_HNZ_00_10665149.vel.V2c"
-prism_dis_file = "/home/jeffsanchez/Downloads/cosmos_downloads/Event_3/prismPROCESSED/CI.10665149/SB.WLA/V2/SB_WLA_HNZ_00_10665149.dis.V2c"
+if event_type == 1:
+    mseed_file     = "/home/jeffsanchez/Downloads/cosmos_downloads/Event_1/evid_15200401_13519/SB_WLA_HNZ_00_15200401.msd"
+    prism_v1_file  = "/home/jeffsanchez/Downloads/cosmos_downloads/Event_1/prismPROCESSED/CI.15200401/SB.WLA/V1/SB_WLA_HNZ_00_15200401.V1c"
+    prism_acc_file = "/home/jeffsanchez/Downloads/cosmos_downloads/Event_1/prismPROCESSED/CI.15200401/SB.WLA/V2/SB_WLA_HNZ_00_15200401.acc.V2c"
+    prism_vel_file = "/home/jeffsanchez/Downloads/cosmos_downloads/Event_1/prismPROCESSED/CI.15200401/SB.WLA/V2/SB_WLA_HNZ_00_15200401.vel.V2c"
+    prism_dis_file = "/home/jeffsanchez/Downloads/cosmos_downloads/Event_1/prismPROCESSED/CI.15200401/SB.WLA/V2/SB_WLA_HNZ_00_15200401.dis.V2c"
+elif event_type == 2:
+    mseed_file     = "/home/jeffsanchez/Downloads/cosmos_downloads/Event_2/evid_14733516_4612/SB_WLA_HNZ_00_14733516.msd"
+    prism_v1_file  = "/home/jeffsanchez/Downloads/cosmos_downloads/Event_2/prismPROCESSED/CI.14733516/SB.WLA/V1/SB_WLA_HNZ_00_14733516.V1c"
+    prism_acc_file = "/home/jeffsanchez/Downloads/cosmos_downloads/Event_2/prismPROCESSED/CI.14733516/SB.WLA/V2/SB_WLA_HNZ_00_14733516.acc.V2c"
+    prism_vel_file = "/home/jeffsanchez/Downloads/cosmos_downloads/Event_2/prismPROCESSED/CI.14733516/SB.WLA/V2/SB_WLA_HNZ_00_14733516.vel.V2c"
+    prism_dis_file = "/home/jeffsanchez/Downloads/cosmos_downloads/Event_2/prismPROCESSED/CI.14733516/SB.WLA/V2/SB_WLA_HNZ_00_14733516.dis.V2c"
+elif event_type == 3:
+    mseed_file     = "/home/jeffsanchez/Downloads/cosmos_downloads/Event_3/evid_10665149_4386/SB_WLA_HNZ_00_10665149.msd"
+    prism_v1_file  = "/home/jeffsanchez/Downloads/cosmos_downloads/Event_3/prismPROCESSED/CI.10665149/SB.WLA/V1/SB_WLA_HNZ_00_10665149.V1c"
+    prism_acc_file = "/home/jeffsanchez/Downloads/cosmos_downloads/Event_3/prismPROCESSED/CI.10665149/SB.WLA/V2/SB_WLA_HNZ_00_10665149.acc.V2c"
+    prism_vel_file = "/home/jeffsanchez/Downloads/cosmos_downloads/Event_3/prismPROCESSED/CI.10665149/SB.WLA/V2/SB_WLA_HNZ_00_10665149.vel.V2c"
+    prism_dis_file = "/home/jeffsanchez/Downloads/cosmos_downloads/Event_3/prismPROCESSED/CI.10665149/SB.WLA/V2/SB_WLA_HNZ_00_10665149.dis.V2c"
 prism_files = (prism_v1_file, prism_acc_file, prism_vel_file, prism_dis_file)
 network = "SB"
 station = "WLA"
 channel = "HNZ"
+
+sta_window = 2
+lta_window = 10
+thresh_on = 1.5
+thresh_off = 1
 
 def get_PRISM_data(filename):
     with open(filename) as f:
@@ -83,24 +99,78 @@ def convert_counts_to_metric_trace(tr, metric_units):
         # Note: The following are applied before deconvolution
         # a bandpass filter
         # a quarter cosine taper, tapering 0.05 length of data
-        tr.remove_response(pre_filt=[0.1, 0.5, 0.95*freq, freq],
+        lowcut = get_low_corner_freq(tr, plot=True)
+        lowcut = lowcut if lowcut <= 0.5 else 0.5
+        print("used lowcut Hz:", lowcut)
+        tr.remove_response(pre_filt=[lowcut, lowcut*1.1, 0.5*freq*0.9, 0.5*freq],
+        #tr.remove_response(pre_filt=[0.1, 0.5, 49, 50],
                            output=metric_units, water_level=4.5, taper=True, taper_fraction=0.1)
         tr.stats.units = metric_units
     return tr
 
-def remove_noise(tr):
+def get_low_corner_freq(tr, event_onset = None, low_power_thresh = 0.015, use_end=False, plot=False):
+    global event_type
+    tr = tr.copy()
+    if event_onset == None:
+        # get event onset
+        freq = tr.stats.sampling_rate
+        sta_lta = recursive_sta_lta(tr, nsta=int(sta_window*freq), nlta=int(lta_window*freq))
+        pick_pairs_ind = trigger_onset(sta_lta, thresh_on, thresh_off)
+        event_onset = pick_pairs_ind[0][0]
 
-    if event_type == 3:
-        freqmin= 0.5
-        freqmax= 25
-    elif event_type == 2:
-        freqmin= 0.3
-        freqmax= 35
-    else:
-        freqmin= 0.1
-        freqmax= 40
-    #tr.filter("bandpass",freqmin=freqmin,freqmax=freqmax) # based on magnitude
-    return tr
+    if use_end:
+        noise = tr.data[-1*int(event_onset-1*tr.stats.sampling_rate):] # remove one second worth of samples
+    else: # get noise from beginning
+        noise = tr.data[:int(event_onset-1*tr.stats.sampling_rate)] # remove one second worth of samples
+    noise = noise - np.mean(noise) #remove DC
+    noise_resp = abs(np.fft.rfft(noise, n=_npts2nfft(len(noise)))) # get distance as magnitude
+    noise_resp_x = np.fft.rfftfreq(_npts2nfft(len(noise)), 1/tr.stats.sampling_rate)
+    nyquist_ind = len(noise_resp)//2
+    noise_integral = integrate.cumtrapz(noise_resp[:nyquist_ind], noise_resp_x[:nyquist_ind], initial=0) # only up to nyquist freq
+    noise_integral = noise_integral/noise_integral[-1] # normalize w max
+    lowcut_ind = int((noise_integral > low_power_thresh).nonzero()[0][0])
+    lowcut = noise_resp_x[lowcut_ind]
+    print("power% centered at lowcut:", noise_integral[lowcut_ind-1:lowcut_ind+2])
+    print("lowcut_ind:",lowcut_ind)
+    print("lowcut Hz:",lowcut)
+
+    # get first occurence of greater than 1/2 threshold
+
+    signal = tr.detrend("linear").data
+    signal_resp = abs(np.fft.rfft(signal, n=_npts2nfft(len(signal)))) # get distance as magnitude
+    signal_resp_x = np.fft.rfftfreq(_npts2nfft(len(signal)), 1/tr.stats.sampling_rate)
+    final_signal = tr.copy()
+    final_signal.remove_response(pre_filt=[lowcut, lowcut*1.1, 49, 50],
+                       output="ACC", water_level=4.5, taper=True, taper_fraction=0.1)
+
+    if plot:
+        fig, axs = plt.subplots(nrows=3, ncols=2, constrained_layout=True)
+        fig.set_size_inches(w=12, h=6)
+
+        fig.suptitle("Event "+str(event_type)+" Noise Removal")
+
+        # NOISE PLOT COLUMN
+        axs[0][0].set_ylabel("Noise\nCounts")
+        axs[0][0].plot(noise)
+        axs[1][0].set_ylabel("Noise Response\nCounts")
+        axs[1][0].plot(noise_resp_x,noise_resp)
+        axs[2][0].set_ylabel("Noise Response\nNormalized Integral")
+        axs[2][0].plot(noise_resp_x[:nyquist_ind],noise_integral)
+
+        # SIGNAL PLOT COLUMN
+        axs[0][1].set_ylabel("Signal\nCounts")
+        axs[0][1].plot(tr.data)
+        axs[1][1].set_ylabel("Signal Response\nCounts")
+        axs[1][1].plot(signal_resp_x,signal_resp)
+        axs[2][1].set_ylabel("Final Signal \nCounts")
+        axs[2][1].plot(final_signal)
+
+        for row in axs:
+            for ax in row:
+                ax.grid()
+        plt.show()
+
+    return lowcut
 
 def improved_integration(tr):
     tr = tr.copy()
@@ -160,11 +230,14 @@ def compare(prism_files, mseed_file, network, station, channel, plot_results = T
     inv = get_inventory(inv_path, network, station)
 
     counts = st.select(channel=channel)[0]
+    #freq = counts.stats.sampling_rate
+    #plot_trigger(counts, recursive_sta_lta(counts, nsta=int(sta_window*freq), nlta=int(lta_window*freq)), 1.5, 1)
     counts.attach_response(inv)
     counts.stats.units = "COUNTS"
     counts_mean = counts.data.mean()
     counts_peak = max(abs(counts.data))
     acc = convert_counts_to_metric_trace(counts, metric_units)
+    return
     acc_mean = acc.data.mean()
     acc_peak = max(abs(acc.data))
     vel = convert_acc_to_vel_trace(acc)
@@ -190,6 +263,7 @@ def compare(prism_files, mseed_file, network, station, channel, plot_results = T
 
     dis_ae = abs(dis.data - prism_dis)
     dis_ae_mean = dis_ae.mean()
+    print(dis_ae_mean)
     dis_ae_peak = max(abs(dis_ae))
 
 
