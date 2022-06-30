f81a0821aca6408a758308ddfe864eaa1b611ee3
Jeff Sanchez (sysop@hp) <sanchez.christopherjeff@gmail.com>
2022-06-30 19:28:05 +0800

    feat: Get more rshake info

--- SLclient.py
+++ SLclient.py
@@ -1,5 +1,6 @@
 import threading
 from obspy.clients.seedlink import basic_client, easyseedlink
+from obspy import UTCDateTime
 
 class SLclient(threading.Thread):
     """ Class to process and forward mseed from given station """
@@ -12,11 +13,11 @@ class SLclient(threading.Thread):
         """
 
         self.station_IP = station_IP
-        self.network, self.station = self._get_info(station_IP)
+        self.network, self.station, self.channels, self.sps = self._get_info(station_IP)
         self.thread_name = self.__class__.__name__ + ":" + self.station
         threading.Thread.__init__(self, name=self.thread_name)
 
-        self.topic = self.station.upper() + " " + "TRACES"
+        self.topic = self.station.upper() + "_" + "TRACES"
         self.message_board = message_board
 
         # setup realtime stream
@@ -27,8 +28,17 @@ class SLclient(threading.Thread):
 
     def _get_info(self,station_IP):
         client = basic_client.Client(station_IP)
-        network, station = client.get_info(level="station")[0]
-        return network, station
+        station_info = client.get_info(level="channel")
+        network = station_info[0][0]
+        station = station_info[0][1]
+        channels = [info[3] for info in station_info]
+
+        # get 3-sec length sample data from 1 minute ago
+        starttime = UTCDateTime() - 60
+        sample_stream = client.get_waveforms(network, station, "??", channels[0], \
+                        starttime, starttime+3)
+        sps = sample_stream[0].stats.sampling_rate # samples per sec
+        return network, station, channels, int(sps)
 
     def _data_pckt_rcvd_callback(self, tr):
         """ Add data to STATION_TRACES topic
